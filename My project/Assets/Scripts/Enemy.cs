377f13fa98c5ce0333d308a848f7b6ccfb083c8d
m305_16\m305 <marlene8810@gmail.com>
2022-11-23 11:35:24 +0800

    no message

--- My project/Assets/Scripts/Enemy.cs
+++ My project/Assets/Scripts/Enemy.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    public float hp = 100;
+    public float maxHp = 100;
+    public Image  hpBar ;
+    public GameObject statusBar; //資訊bar
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,28 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 offset = new Vector3(0, 50, 0);
+        Vector3 p = Camera.main.WorldToScreenPoint(transform.position);
+        statusBar.transform.position = p + offset;
+            
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bullet")
+        {
+            hp -= 25;//扣血
+
+
+            //控制hp bar
+            float sx = hp / maxHp;
+            hpBar.rectTransform.localScale = new Vector3(sx, 1, 1);
+            //血沒了移除自己
+
+            if (hp <= 0)
+            {
+                gameObject.SetActive(false);
+                Destroy(gameObject);
+            }
+        }
     }
 }
